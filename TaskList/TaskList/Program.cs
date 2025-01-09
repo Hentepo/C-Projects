083a4b6fcdcedbcd4a970bccee361b72c332ef29
Hentepo <hannihe4@gmail.com>
2025-01-09 13:49:49 +0200

    git commit test 3

---
+++ TaskList/TaskList/Program.cs
@@ -0,0 +1,15 @@
+﻿
+Console.ForegroundColor = ConsoleColor.White;
+Console.WriteLine("Welcome to MyTaskList!");
+
+Console.WriteLine("Your current tasks are: ");
+
+
+List<string> list = new List<string>();
+list.Add("Test1");
+list.Add("Test2");
+list.Add("Test3");
+
+
+foreach (string item in list)
+    Console.WriteLine(item);
