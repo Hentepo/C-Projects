414e1715c388426e7f27071d9c866f54db01026a
Hentepo <hannihe4@gmail.com>
2025-01-11 10:14:12 +0200

    MetalMusicQuiz initial commit

---
+++ MetalMusicQuiz/MetalMusicQuiz/Utility.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MetalMusicQuiz
+{
+    public class Utility
+    {
+
+        public string GetUserInput(string prompt)
+        {
+            Console.WriteLine($"Enter {prompt}");
+            return Console.ReadLine();
+        }
+
+        public void PressEnterToContinue()
+        {
+            Console.WriteLine("Press Enter to Continue");
+            Console.ReadLine();
+        }
+
+    }
+}
