bc333d733d44b24b40b24128eaef5b45ede3f264
Hentepo <hannihe4@gmail.com>
2025-01-13 10:12:41 +0200

    MyWeatherApp initial commit

---
+++ MyWeatherApp/MyWeatherApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using MyWeatherApp;
+
+Console.ForegroundColor = ConsoleColor.White;
+
+Utility.Welcome();
+Utility.PressEnterToContinue();
+
+while (true)
+{
+    Utility.DisplayMenu();
+
+    try
+    {
+        int option = Convert.ToInt32(Console.ReadLine());
+
+        if (option == 2)
+        {
+            Console.WriteLine("See you next time!");
+            break;
+        }
+    }
+    catch 
+    {
+        continue;
+    }
+}
---
+++ MyWeatherApp/MyWeatherApp/Utility.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyWeatherApp
+{
+    public static class Utility
+    {
+        public static void Welcome()
+        {
+            Console.WriteLine("--------MyWeatherApp--------");
+            Console.WriteLine("Welcome to MyWeatherApp!");
+            Console.WriteLine();
+        }
+
+        public static void DisplayMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("--------MyWeatherApp--------");
+            Console.WriteLine("1. Check Weather            :");
+            Console.WriteLine("2. Exit                     :");
+        }
+
+        public static void PressEnterToContinue()
+        {
+            Console.WriteLine("Press Enter to Continue");
+            Console.ReadLine();
+        }
+
+
+    }
+}
