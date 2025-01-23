04ef4bf4784690a78faeeed30f79cb22eb632912
Hentepo <hannihe4@gmail.com>
2025-01-23 13:24:25 +0200

    LibraryDatabaseProject Initial Commit

---
+++ LibraryDatabaseProject/AppScreen.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryDatabaseProject
+{
+    internal class AppScreen
+    {
+        static public void Welcome()
+        {
+            Console.WriteLine("---------------MyLibrary---------------");
+            Console.WriteLine("Welcome to MyLibrary!");
+            Console.WriteLine();
+        }
+
+        static public void Menu()
+        {
+            Console.WriteLine("---------------MyLibrary---------------");
+            Console.WriteLine("1. Show My Books                      :");
+            Console.WriteLine("2. Add Book                           :");
+            Console.WriteLine("3. Delete Book                        :");
+            Console.WriteLine("4. Exit                               :");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ LibraryDatabaseProject/Utilities.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryDatabaseProject
+{
+    internal class Utilities
+    {
+        public static string GetUserInput(string prompt)
+        {
+            Console.WriteLine($"Enter {prompt}");
+            return Console.ReadLine();
+        }
+
+        public static void PressEnterToContinue()
+        {
+            Console.WriteLine("Press Enter to Continue");
+            Console.ReadLine();
+        }
+
+        public static void DisplayMessage(string message, bool success)
+        {
+            if (success)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+            }
+            Console.WriteLine();
+            Console.WriteLine(message);
+            Console.ForegroundColor = ConsoleColor.White;
+            PressEnterToContinue();
+        }
+    }
+}
